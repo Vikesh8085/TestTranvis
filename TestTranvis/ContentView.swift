03752351f640a251a8a60f30490d21adbf5b40ab
vikesh <vikesh.prasad@mobiotics.com>
2020-08-10 19:41:12 +0530

    Initial Commit

---
+++ TestTranvis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTranvis
+//
+//  Created by Vikesh Prasad on 10/08/20.
+//  Copyright © 2020 vikeshapps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
